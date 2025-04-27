3564dd86a25d494fd6e84fd3bb8148cda255d43a
mingshen0118 <129404655+mingshen0118@users.noreply.github.com>
2025-04-27 14:48:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,11 @@ reviews = load_reviews()
 #TFIDF
 @st.cache_resource
 def precompute_tfidf(recipes):
+    recipes['text_features'] = (
+        recipes['RecipeIngredientParts'].fillna('').astype(str) + " " +
+        recipes['Keywords'].fillna('').astype(str)
+    )
+    
     tfidf_model = TfidfVectorizer(
         stop_words='english',
         max_features=5000,
